372c0e895119db3cbb2f02bd31b7616c28a59118
priya <priya97a@gmail.com>
2017-08-06 17:40:46 +0530

    dynamic functionality added

--- game-2/src/EasyABC.js
+++ game-2/src/EasyABC.js
@@ -11,14 +11,54 @@ export default class EasyABC extends Component{
             currentTick :0
         };
         this.next= this.next.bind(this);
+        this.prev= this.prev.bind(this);
+        this.playSound = this.playSound.bind(this);
     }
-    next(){
-        if(this.state.currentTick <2){
-            this.setState({currentTick:this.state.currentTick+1})
-        }else{
-            this.setState({currentPosition:this.state.currentPosition+1,currentTick:0})
+    componentDidMount(){
+        let letterSound = document.querySelector(`audio[data-key="letter"]`)
+        if(this.state.currentPosition ===0){
+            letterSound.currentTime = 0;
+            letterSound.play();
         }
 
+    }
+    componentDidUpdate(){
+        this.playSound();
+    }
+    playSound(){
+        let letterSound = document.querySelector(`audio[data-key="letter"]`)
+        let wordSound = document.querySelector(`audio[data-key="word"]`)
+
+        if(this.state.currentTick===0){
+            letterSound.currentTime=0;
+            letterSound.play();
+        }else{
+            wordSound.currentTime=0;
+            wordSound.play();
+        }
+    }
+    next(){
+        if(this.state.currentPosition === this.state.alphabets.length-1){
+            this.setState({currentPosition:0,currentTick:0})
+            if(this.state.currentTick <2){
+                this.setState({currentTick:this.state.currentTick+1})
+            }else{
+                this.setState({currentPosition:0,currentTick:0})
+            }
+        }else{
+            if(this.state.currentTick <2){
+                this.setState({currentTick:this.state.currentTick+1})
+            }else{
+                this.setState({currentPosition:this.state.currentPosition+1,currentTick:0})
+            }
+        }
+    }
+    prev(){
+        if(this.state.currentPosition > 0){
+            this.setState({currentPosition:this.state.currentPosition-1})
+        }else{
+            this.setState({currentPosition:this.state.alphabets.length-1})
+        }
     }
     render(){
         let showImage =this.state.currentTick !== 0? true : false;
@@ -30,10 +70,12 @@ export default class EasyABC extends Component{
                         <div className="field-block">
                             {this.state.alphabets[this.state.currentPosition].letter}
                         </div>
+                        <audio src={this.state.alphabets[this.state.currentPosition].letterSound}
+                               data-key="letter"/>
                     </div>
                     <div className="buttons">
-                        <a className="button prev">Previous</a>
-                        <a className="button sound">Play Sound Again</a>
+                        <a onClick={this.prev}className="button prev">Previous</a>
+                        <a onClick={this.playSound} className="button sound">Play Sound Again</a>
                         <a onClick={this.next} className="button next">Next</a>
                     </div>
                     <div className="fields">
@@ -43,6 +85,8 @@ export default class EasyABC extends Component{
                                 <img className={classNames('letter-image', {hide: !showImage})}
                                     alt={this.state.alphabets[this.state.currentPosition].word}
                                     src={this.state.alphabets[this.state.currentPosition].image}/>
+                                <audio src={this.state.alphabets[this.state.currentPosition].wordSound}
+                                           data-key="word"/>
                             </div>
                             <div className="right-field">
                                 <div className={classNames('placeholder-span', {hide: showWord})}>Click next to here spelling</div>
